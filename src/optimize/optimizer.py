1557476e588d7568dcddc1ab499311c9b141eb7c
pivote1214 <yuta.m.12.1214@gmail.com>
2023-11-23 22:21:48 +0900

    optimizer

---
+++ src/optimize/optimizer.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TypeVar
+
+from src.optimize.params import Parameter
+from src.utils.paths import OPT_MODEL_DIR
+from src.utils.module_handler import get_object_from_module
+
+IndexSet = TypeVar("IndexSet")
+Constant = TypeVar("Constant")
+Model = TypeVar("Model")
+
+
+@dataclass
+class ModelInput:
+    model_name: str
+    index_set: IndexSet
+    constant: Constant
+
+
+class Optimizer:
+    def __init__(
+        self,
+        model_name: str, 
+        params: Parameter
+    ):
+        self.model_name = model_name
+        self.params = params
+        self.result = {}
+
+    @staticmethod
+    def make_model_input(
+        model_name: str, 
+        params: Parameter
+    ) -> ModelInput:
+        """Generate model input"""
+        module_path = OPT_MODEL_DIR / model_name / "make_input.py"
+        make_input = get_object_from_module(module_path, f"make_input")
+        index_set, constant = make_input(params=params)
+        model_input = ModelInput(model_name=model_name, index_set=index_set, constant=constant)
+        return model_input
+
+    @staticmethod
+    def make_model(model_input: ModelInput) -> Model:
+        """Bulid optimization model"""
+        module_path = OPT_MODEL_DIR / model_input.model_name / "model.py"
+        model_class = get_object_from_module(module_path, "Model")
+        model = model_class(index_set=model_input.index_set, constant=model_input.constant)
+        return model
+
+    def run(self) -> None:
+        model_input = Optimizer.make_model_input(
+            model_name=self.model_name, params=self.params
+        )
+        model = Optimizer.make_model(model_input)
+        model.solve(self.params.TimeLimit)
+        self.result['params'] = self.params
+        self.result['result'] = model.result
+
+        return self.result
