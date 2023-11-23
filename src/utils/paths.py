30500af7db4ad39745945305c2c96c99c3de3e19
pivote1214 <yuta.m.12.1214@gmail.com>
2023-11-23 22:20:48 +0900

    add

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent.parent.absolute()
+DATA_DIR = ROOT_DIR / 'data'
+PATHS_DIR = DATA_DIR / 'input' / 'paths'
 RESULT_DIR = ROOT_DIR / 'results'
 SRC_DIR = ROOT_DIR / 'src'
+OPT_MODEL_DIR = SRC_DIR / 'optimize' / 'models'
