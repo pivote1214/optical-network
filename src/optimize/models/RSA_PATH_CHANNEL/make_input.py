9f4bfa59de8e8b9002d93ffb9c67cf7d262e3d9c
pivote1214 <yuta.m.12.1214@gmail.com>
2023-11-23 22:21:07 +0900

    RSA_PATH_CHANNEL

---
+++ src/optimize/models/RSA_PATH_CHANNEL/make_input.py
@@ -0,0 +1,149 @@
+from __future__ import annotations
+
+from typing import Dict, List
+
+import pickle
+import pprint
+import numpy as np
+
+from src.utils.paths import PATHS_DIR
+from src.utils.graph import calc_path_length, is_edge_in_path
+from src.demands.demands import gen_all_demands_offline
+from src.optimize.models.RSA_PATH_CHANNEL.model import Constant, IndexSet
+from src.optimize.params import Parameter
+
+
+def make_input(params: Parameter) -> tuple[IndexSet, Constant]:
+    """Generate index set and constant"""
+    # make index set
+    E = {e_ind: edge for e_ind, edge in enumerate(params.graph.edges)}
+    S = [s_ind for s_ind in range(params.num_slots)]
+    D = gen_all_demands_offline(params.graph, params.num_demands, 
+                                seed=params.demands_seed)
+    all_paths_dir = PATHS_DIR / f"{params.path_algo_name}_{params.network_name}_k={params.k}_alpha={params.alpha}.pickle"
+    with open(all_paths_dir, mode="rb") as f:
+        paths_info = pickle.load(f)
+        all_paths = paths_info["all_paths"]
+    P = _make_path(D, all_paths)
+    C = _make_channels(params, S, D, P)
+    index_set = IndexSet(E=E, S=S, D=D, P=P, C=C)
+
+    # make constant
+    delta = _calculate_delta(E, D, P)
+    gamma = _calculate_gamma(S, D, P, C)
+    constant = Constant(delta=delta, gamma=gamma)
+
+    return index_set, constant
+
+
+def _make_path(
+    D: Dict[int, tuple[int, int, int]], 
+    all_paths: Dict[tuple(int, int), List[List[int]]]
+    ) -> Dict[int, List[List[int]]]:
+    """Generate path set"""
+    path_set = {}
+    for d_ind in D.keys():
+        source, destination, _ = D[d_ind]
+        path_set[d_ind] = all_paths[source, destination]
+
+    return path_set
+
+
+def _make_channels(
+    params: Parameter, 
+    S: List[int], 
+    D: Dict[int, tuple[int, int, int]], 
+    P: Dict[int, List[List[int]]]
+    ) -> tuple[Dict[tuple[int, int], int], Dict[tuple[int, int], List[List[int]]] ]:
+    """Generate channel set"""
+    max_slot = len(S)
+    channels = {}
+    for d_ind, demand in D.items():
+        for p_ind, path in enumerate(P[d_ind]):
+            # select modulation format
+            path_length = calc_path_length(params.graph, path)
+            modulation_format = _select_modulation_format(path_length)
+            # calculate required slots
+            required_slots = _calc_required_slots(demand[2], modulation_format, 
+                                                  params.W, params.traffic_bpsk)
+            channels[d_ind, p_ind] = _calc_candidate_channel(required_slots, max_slot)
+
+    return channels
+
+
+def _calc_candidate_channel(slot_num: int, max_slot: int) -> List[List[int]]:
+    """Calculate candidate channels"""
+    channels = []
+    for i in range(max_slot - slot_num + 1):
+        channel = []
+        for j in range(slot_num):
+            channel.append(i + j)
+        channels.append(channel)
+
+    return channels
+
+
+def _select_modulation_format(path_length: int) -> int:
+    """Select modulation format"""
+    if path_length <= 600:
+        modulation_format = 4
+    elif path_length <= 1200:
+        modulation_format = 3
+    elif path_length <= 3500:
+        modulation_format = 2
+    elif path_length <= 6300:
+        modulation_format = 1
+    else:
+        raise ValueError("Path length is too long.")
+
+    return modulation_format
+
+
+def _calc_required_slots(
+    demand_size: float, 
+    modulation_format: int, 
+    W: dict[str, float], 
+    traffic_bpsk: float
+    ) -> int:
+    """Calculate required slots"""
+    # required_slots = np.ceil(
+    #     np.ceil(
+    #         (demand_size / (modulation_format * traffic_bpsk)) * \
+    #         W['OC'] + 2 * W['GB']
+    #         ) / W['FS']
+    #         )
+    required_slots = np.ceil(demand_size / (modulation_format * traffic_bpsk)) * 3 + 1
+    required_slots = int(required_slots)
+
+    return required_slots
+
+
+def _calculate_delta(
+    E: dict[int, tuple[int, int]], 
+    D: dict[int, tuple[int, int, int]], 
+    P: dict[int, List[List[int]]]
+    ) -> Dict[tuple[int, int, int], int]:
+    """calculate delta (path contains edge or not)"""
+    delta = {}
+    for e_ind, edge in E.items():
+        for d_ind, _ in D.items():
+            for p_ind, path in enumerate(P[d_ind]):
+                delta[e_ind, d_ind, p_ind] = is_edge_in_path(path, edge)
+
+    return delta
+
+def _calculate_gamma(
+    S: List[int], 
+    D: Dict[int, tuple[int, int, int]], 
+    P: Dict[int, List[List[int]]], 
+    C: Dict[tuple[int, int], List[List[int]]], 
+    ) -> Dict[tuple[int, int, int, int], int]:
+    """calculate gamma (channel contains slice or not)"""
+    gamma = {}
+    for d_ind, _ in D.items():
+        for p_ind, path in enumerate(P[d_ind]):
+            for c_ind, channel in enumerate(C[d_ind, p_ind]):
+                for s_ind, slice in enumerate(S):
+                    gamma[d_ind, p_ind, c_ind, s_ind] = 1 if slice in channel else 0
+
+    return gamma
---
+++ src/optimize/models/RSA_PATH_CHANNEL/model.py
@@ -0,0 +1,152 @@
+from typing import Dict, List, Tuple, Optional
+
+import time
+import pprint
+import gurobipy as gp
+from dataclasses import dataclass
+from pathlib import Path
+
+from src.utils.paths import DATA_DIR
+from src.optimize.result import OptResult
+
+
+@dataclass(frozen=True)
+class IndexSet:
+    E: Dict[int, Tuple[int, int]]
+    S: List[int]
+    D: Dict[int, Tuple[int, int, int]]
+    P: Dict[int, List[List[int]]]
+    C: Dict[Tuple[int, int], List[List[int]]]
+
+
+@dataclass(frozen=True)
+class Constant:
+    delta: Dict[Tuple[int, int, int], int]
+    gamma: Dict[Tuple[int, int, int, int], int]
+
+
+class Model:
+    def __init__(self, index_set: IndexSet, constant: Constant):
+        self.index_set = index_set
+        self.constant = constant
+        self.name = 'RSA_PATH_CHANNEL'
+        self.result = None
+        self.calculation_time = None
+        self.objective = None
+
+    def _set_model(self) -> gp.Model:
+        problem = gp.Model(self.name)
+        # set variables
+        self.x, self.y_es, self.y_s = {}, {}, {}
+        # set variables x
+        for d_ind, _ in self.index_set.D.items():
+            for p_ind, _ in enumerate(self.index_set.P[d_ind]):
+                for c_ind, _ in enumerate(self.index_set.C[d_ind, p_ind]):
+                    self.x[d_ind, p_ind, c_ind] = problem.addVar(
+                        vtype=gp.GRB.BINARY, name=f'x_{d_ind}_{p_ind}_{c_ind}'
+                    )
+        # set variables y_es
+        for e_ind, _ in self.index_set.E.items():
+            for s_ind, _ in enumerate(self.index_set.S):
+                self.y_es[e_ind, s_ind] = problem.addVar(
+                    vtype=gp.GRB.BINARY, name=f'y_es_{e_ind}_{s_ind}'
+                )
+        # set variables y_s
+        for s_ind, _ in enumerate(self.index_set.S):
+            self.y_s[s_ind] = problem.addVar(
+                vtype=gp.GRB.BINARY, name=f'y_s_{s_ind}'
+            )
+        # update variables
+        problem.update()
+
+        # set objective function
+        problem.setObjective(
+            gp.quicksum(self.y_s[s_ind] * s_ind for s_ind in self.index_set.S), gp.GRB.MINIMIZE
+            # gp.quicksum(self.y_s[s_ind] for s_ind in self.index_set.S), gp.GRB.MINIMIZE
+        )
+        # update objective function
+        problem.update()
+
+        # set constraints
+        # set nonoverlap constraint
+        for d_ind, _ in self.index_set.D.items():
+            problem.addConstr(
+                gp.quicksum(
+                    self.x[d_ind, p_ind, c_ind] 
+                    for p_ind, _ in enumerate(self.index_set.P[d_ind]) 
+                    for c_ind, _ in enumerate(self.index_set.C[d_ind, p_ind])
+                ) 
+                == 1
+            )
+        # set y_es constraint
+        for e_ind, _ in self.index_set.E.items():
+            for s_ind, _ in enumerate(self.index_set.S):
+                problem.addConstr(
+                    gp.quicksum(
+                        self.x[d_ind, p_ind, c_ind] * \
+                            self.constant.gamma[d_ind, p_ind, c_ind, s_ind] * \
+                                self.constant.delta[e_ind, d_ind, p_ind]
+                            for d_ind, _ in self.index_set.D.items() 
+                            for p_ind, _ in enumerate(self.index_set.P[d_ind]) 
+                            for c_ind, _ in enumerate(self.index_set.C[d_ind, p_ind])
+                    ) 
+                    <= self.y_es[e_ind, s_ind]
+                )
+        # set y_s constraint
+        for s_ind, _ in enumerate(self.index_set.S):
+            problem.addConstr(
+                gp.quicksum(
+                    self.y_es[e_ind, s_ind] 
+                    for e_ind, _ in self.index_set.E.items()
+                ) 
+                <= len(self.index_set.E) * self.y_s[s_ind]
+            )
+        # update constraints
+        problem.update()
+
+        return problem
+
+    def variables_to_value(self) -> None:
+        self.x = {key: var.X for key, var in self.x.items()}
+        self.y_es = {key: var.X for key, var in self.y_es.items()}
+        self.y_s = {key: var.X for key, var in self.y_s.items()}
+
+    def calculate_used_slots(self) -> int:
+        used_slots = 0
+        for s_ind, _ in enumerate(self.index_set.S):
+            used_slots += self.y_s[s_ind].X
+        return used_slots
+    
+    def write_lp(self, dir_path: Optional[Path] = DATA_DIR / 'output' / 'lpfile') -> None:
+        self.problem.write_lp(str(dir_path / f'{self.name}.lp'))
+
+    def solve(
+        self, 
+        TimeLimit: Optional[int] = 3600, 
+        write_lp: bool = False
+        ) -> None:
+        # set model
+        self.problem = self._set_model()
+        # set time limit
+        self.problem.setParam('TimeLimit', TimeLimit)
+        self.problem.setParam('OutputFlag', 0)
+
+        # solve
+        start = time.time()
+        self.problem.optimize()
+        elapsed_time = time.time() - start
+        # store result
+        self.objective = self.problem.ObjVal
+        self.used_slots = self.calculate_used_slots()
+        self.variables_to_value()
+        self.result = OptResult(
+            calculation_time=elapsed_time, 
+            objective=self.objective, 
+            used_slots=self.used_slots, 
+            problem=self.problem, 
+            index_set=self.index_set, 
+            constant=self.constant, 
+            variable={'x': self.x, 'y_es': self.y_es, 'y_s': self.y_s}
+            )
+        if write_lp:
+            self.write_lp()
