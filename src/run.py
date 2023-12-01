b7857b6ce693c99ff6cfe5605cb2f4b1cc888431
pivote1214 <yuta.m.12.1214@gmail.com>
2023-12-01 12:09:22 +0900

    experiment03

--- src/run.py
+++ src/run.py
@@ -16,13 +16,10 @@ if __name__ == "__main__":
     model_name = 'RSA_PATH_CHANNEL'
     network_name = 'NSF'
     graph = load_network(network_name)
-    num_slots = 90
-    num_demands = 30
+    num_slots = 275
+    num_demands = 100
     k_values = [2, 3, 5]
     algo_and_alpha = [('kSP', None), ('kSPwLO', 0.3), ('kSPwLO', 0.5)]
-    # # Test
-    # num_slots = 30
-    # num_demands = 6
 
     # write global config
     with open(RESULT_DIR / f'experiment{experiment_num}/global_config.txt', 'w') as f:
@@ -38,23 +35,28 @@ if __name__ == "__main__":
     algo_columns = ['seed'] + [f'{algo}_{alpha}' for algo, alpha in algo_and_alpha]
     # metrics-algo_columnsのmulti-columnを作成
     columns = pd.MultiIndex.from_product([metrics, algo_columns])
-    index = pd.MultiIndex.from_product([k_values, [seed * 50 for seed in range(1, 11)]])
+    index = pd.MultiIndex.from_product([k_values, [seed * 12 for seed in range(1, 11)]])
     result_table = pd.DataFrame(index=index, columns=columns)
-    for k in tqdm(k_values):
-        for path_algo_name, alpha in tqdm(algo_and_alpha, leave=False):
+    # for k in tqdm(k_values):
+    #     for path_algo_name, alpha in tqdm(algo_and_alpha, leave=False):
+    #         sum_used_slots, sum_time = 0, 0
+    #         times = 10
+    #         for seed in tqdm(range(1, 11), leave=False):
+    for k in k_values:
+        for path_algo_name, alpha in algo_and_alpha:
             sum_used_slots, sum_time = 0, 0
             times = 10
-            for seed in tqdm(range(1, 11), leave=False):
+            for seed in range(1, 11):
                 params = Parameter(
                     network_name=network_name, 
                     graph=graph, 
                     num_slots=num_slots, 
                     num_demands=num_demands, 
-                    demands_seed=seed*50, 
+                    demands_seed=seed*12, 
                     k=k, 
                     path_algo_name=path_algo_name, 
                     alpha=alpha, 
-                    TimeLimit=3600
+                    TimeLimit=600
                     )
                 optimizer = Optimizer(model_name=model_name, params=params)
                 result = optimizer.run()
@@ -65,9 +67,9 @@ if __name__ == "__main__":
                     sum_used_slots += result['OptResult'].used_slots
                     sum_time += result['OptResult'].calculation_time
                 # save results
-                result_table.loc[(k, seed*50), ('used_slots', 
+                result_table.loc[(k, seed*12), ('used_slots', 
                                  f'{path_algo_name}_{alpha}')] = result['OptResult'].used_slots
-                result_table.loc[(k, seed*50), ('calculation_time',
+                result_table.loc[(k, seed*12), ('calculation_time',
                                 f'{path_algo_name}_{alpha}')] = result['OptResult'].calculation_time
 
                 # save result_table
