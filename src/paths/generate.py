4caa8bf90b5c30229ee836291e324562c0cf7fa2
pivote1214 <yuta.m.12.1214@gmail.com>
2023-12-01 12:13:18 +0900

    n6s9

--- src/paths/generate.py
+++ src/paths/generate.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     # ダミー
     graph = gp.Model()
     # グラフの定義
-    network_name = "NSF"
+    network_name = "N6S9"
     graph = load_network(network_name)
 
     # パラメータの定義
