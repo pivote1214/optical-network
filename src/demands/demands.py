96d39a86b0fa756b7e4cebd53f72faa3b549a2ad
pivote1214 <yuta.m.12.1214@gmail.com>
2023-12-13 13:02:28 +0900

    give population

--- src/demands/demands.py
+++ src/demands/demands.py
@@ -1,32 +1,27 @@
 from __future__ import annotations
 
+from typing import List
+
 import numpy as np
 import networkx as nx
 
 
-def gen_one_demand_size(
-    generation_principle: str, 
-    min_size: int, 
-    max_size: int
+def _gen_one_demand_size(
+    demands_population: List[int], 
     ) -> int:
     """
-    minsizeからmax_sizeまでのサイズで，1つの通信要求のサイズをランダムに生成する
+    generate one demand size from demands_population
     """
-    if generation_principle == 'Random':
-        return int(np.random.randint(min_size, max_size))
-    if generation_principle == 'Gaussian':
-        return int(np.random.normal(min_size, max_size))
+    return np.random.choice(demands_population)
 
 
 def gen_all_demands_offline(
     graph: nx.Graph, 
     N_DEMANDS: int, 
-    generation_principle: str = 'Random', 
-    min_size: int = 10, 
-    max_size: int = 400, 
+    demands_population: List[int], 
     seed: int = None
     ) -> dict:
-    """N_DEMANDSの数だけ通信要求を生成する"""
+    """gemerate all demands offline"""
     nodes = list(graph.nodes)
     demands = {}
 
@@ -35,7 +30,7 @@ def gen_all_demands_offline(
     
     for i in range(1, N_DEMANDS+1):
         source, destination = np.random.choice(nodes, 2, replace=False)
-        size = gen_one_demand_size(generation_principle, min_size, max_size)
+        size = _gen_one_demand_size(demands_population)
         demands[i] = (source, destination, size)
 
     return demands
