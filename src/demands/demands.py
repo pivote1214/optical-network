a9fd8dc86e1c438a82e1f5acb13180d533bf1edc
pivote1214 <yuta.m.12.1214@gmail.com>
2024-01-22 16:24:10 +0900

    1/22

--- src/demands/demands.py
+++ src/demands/demands.py
@@ -1,13 +1,9 @@
-from __future__ import annotations
-
-from typing import List
-
 import numpy as np
 import networkx as nx
 
 
 def _gen_one_demand_size(
-    demands_population: List[int], 
+    demands_population: list[int], 
     ) -> int:
     """
     generate one demand size from demands_population
@@ -18,7 +14,7 @@ def _gen_one_demand_size(
 def gen_all_demands_offline(
     graph: nx.Graph, 
     N_DEMANDS: int, 
-    demands_population: List[int], 
+    demands_population: list[int], 
     seed: int = None
     ) -> dict:
     """gemerate all demands offline"""
@@ -34,3 +30,16 @@ def gen_all_demands_offline(
         demands[i] = (source, destination, size)
 
     return demands
+
+if __name__ == "__main__":
+    import pickle
+    from src.utils.graph import load_network
+    from src.utils.paths import DATA_DIR
+
+    graph = load_network("NSF")
+    n_demands = 5
+    populations = [50, 100, 150, 200]
+    for seed in range(1, 11):
+        demands = gen_all_demands_offline(graph, n_demands, populations, seed*12)
+        with open(f"{DATA_DIR}/input/demands/NSFNET/demands_{seed}_num={n_demands}_p={populations}.pickle", "wb") as f:
+            pickle.dump(demands, f)
