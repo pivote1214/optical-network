b2ec4a66e1dfebd58108468452fed200569f0b8c
pivote1214 <yuta.m.12.1214@gmail.com>
2024-01-22 16:30:49 +0900

    greedy

--- src/optimize/algorithms/greedy.py
+++ src/optimize/algorithms/greedy.py
@@ -1,4 +1,20 @@
 def greedy_RMLSA_static(demands: dict[int, tuple[int, int, int]]):
     """Greedy RMLSA algorithm for static traffic"""
-    for d_ind, demand in demands.items():
-        source, destination, traffic = demand
+    for d_ind in demands:
+        s, t, size = demands[d_ind]
+        path_d = find_paths(s, t)
+        assined_slots = first_fit(path_d, size)
+        renew_network(assined_slots)
+
+
+def find_paths(s: int, t: int):
+    """Find paths between s and t"""
+    pass
+
+def first_fit(path_d: list[list[int]], size: int):
+    """First fit algorithm"""
+    pass
+
+def renew_network(assined_slots: list[int]):
+    """Renew network"""
+    pass
