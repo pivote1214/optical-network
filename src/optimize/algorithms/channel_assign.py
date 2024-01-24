b6dee4929fe972d34259b9a65cbe129b9159d478
pivote1214 <yuta.m.12.1214@gmail.com>
2024-01-24 17:01:26 +0900

    add first fit

--- src/optimize/algorithms/channel_assign.py
+++ src/optimize/algorithms/channel_assign.py
@@ -1,10 +1,52 @@
 from src.optimize.algorithms.optical_network import OpticalNetwork
+from src.utils.graph import calc_path_length
+from src.optimize.models.RSA_PATH_CHANNEL.make_input import calc_required_slots, select_modulation_format
 
 def first_fit(
     optical_network: OpticalNetwork, 
-    paths_d: list[list[int]], 
-    size: int
-    ) -> tuple[list[int], list[int]]:
+    candidate_paths: list[list[int]], 
+    demand_size: int
+    ) -> tuple[list[tuple[int, int]], list[int]]:
     """First fit algorithm"""
-    slots_availability = optical_network.get_slots_availability(paths_d)
-    
+    # find available slots
+    slots_availability = optical_network.get_slots_availability(candidate_paths)
+    # first fit
+    first_fit_slots = [None] * len(candidate_paths)
+    for p_ind, path in enumerate(candidate_paths):
+        path_length = calc_path_length(optical_network.graph, path)
+        modulation_format = select_modulation_format(path_length)
+        required_slots = calc_required_slots(demand_size, modulation_format, 
+                                             optical_network.W, 
+                                             optical_network.TRAFFIC_BPSK)
+        success_slots = []
+        for s_ind in range(optical_network.num_slots - required_slots + 1):
+            if not slots_availability[p_ind][s_ind]:
+                success_slots = []
+            else:
+                success_slots.append(s_ind)
+                if len(success_slots) == required_slots:
+                    first_fit_slots[p_ind] = success_slots
+                    break
+
+    # find first fit path
+    selected_path_ind, min_ind = None, 1000
+    for p_ind, path in enumerate(candidate_paths):
+        if first_fit_slots[p_ind] is None:
+            continue
+        if first_fit_slots[p_ind][0] < min_ind:
+            selected_path_ind = p_ind
+            min_ind = first_fit_slots[p_ind][0]
+
+    # if no path is selected
+    if selected_path_ind is None:
+        print("No path is selected")
+        return None, None
+
+    # make assigned edges and slots
+    assigned_edges = []
+    for u, v in zip(candidate_paths[selected_path_ind][:-1], 
+                    candidate_paths[selected_path_ind][1:]):
+        assigned_edges.append((u, v))
+    assigned_slots = first_fit_slots[selected_path_ind]
+
+    return assigned_edges, assigned_slots
