42625cdc7a87c7eaf74cd0027c2bd84e587e23e4
Mori Yuta <yuta.m.12.1214@gmail.com>
2024-08-30 17:52:19 +0900

    add test dir

--- utils/namespaces.py
+++ utils/namespaces.py
@@ -1,6 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+TEST_DIR = os.path.join(ROOT_DIR, 'test')
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 PATHS_DIR = os.path.join(DATA_DIR, 'paths')
 NETWORK_DIR = os.path.join(DATA_DIR, 'network')
